9eb684c233664531ed712cbc88a457ee3b207010
eee2w <909623744@qq.com>
2026-02-12 11:16:18 +0800

    Update guide.py

--- guide.py
+++ guide.py
@@ -212,6 +212,13 @@ apps = [
         "description": "锻造石、金装和专武碎片消耗计算",
         "icon": "🔨",
         "status": "online"
+    },
+    {
+        "name": "守城车头卡秒顺序计算器",
+        "url": "https://sanbing-n35z7a8jpimpe3q5xkdyyc.streamlit.app/",
+        "description": "根据对方车头攻城顺序计算己方防守车头卡秒顺序（建议电脑端使用）",
+        "icon": "🛡",
+        "status": "online"
     }
 ]
 
