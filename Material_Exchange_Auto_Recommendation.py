3bab4ae19e4526cd381590076eb4b2dfcfa8cc41
eee2w <909623744@qq.com>
2026-03-14 14:13:11 +0800

    Update Material_Exchange_Auto_Recommendation.py

--- Material_Exchange_Auto_Recommendation.py
+++ Material_Exchange_Auto_Recommendation.py
@@ -4,7 +4,7 @@ import pandas as pd
 # ============= Streamlit 网页应用 =============
 #材料自动兑换计算-Material Exchange Auto-Recommendation
 st.set_page_config(page_title="神兵玉石自动升级计算器", layout="wide")
-st.title("⚔️💎 神兵玉石自动升级计算器-新春版")
+st.title("⚔️💎 神兵玉石自动升级计算器-蜀道奇谋")
 st.info("""
 1、点击左上角双箭头填写积分和材料数量  
 2、选择步兵比弓兵神兵玉石高多少级（默认神兵5级玉石2级）  
@@ -43,11 +43,11 @@ with st.sidebar:
     CURRENT_UNPOLISHED_JADE = st.number_input("璞玉数量", min_value=0, value=0, step=1)
     
     st.subheader("兑换比例")
-    POINTS_PER_WOOD = st.number_input("木头兑换比例", min_value=0.0, value=0.084, step=0.1, format="%.3f")
-    POINTS_PER_MITHRIL = st.number_input("精金兑换比例", min_value=0.0, value=1.67, step=0.1, format="%.2f")
-    POINTS_PER_LAPIS = st.number_input("青金石兑换比例", min_value=0.0, value=5.0, step=0.1, format="%.2f")
-    POINTS_PER_CARVING_KNIFE = st.number_input("琢玉刀兑换比例", min_value=0.0, value=25.0, step=1.0, format="%.2f")
-    POINTS_PER_UNPOLISHED_JADE = st.number_input("璞玉兑换比例", min_value=0.0, value=5.0, step=0.1, format="%.2f")
+    POINTS_PER_WOOD = st.number_input("木头兑换比例", min_value=0.0, value=0.417, step=0.1, format="%.3f")
+    POINTS_PER_MITHRIL = st.number_input("精金兑换比例", min_value=0.0, value=8.34, step=0.1, format="%.2f")
+    POINTS_PER_LAPIS = st.number_input("青金石兑换比例", min_value=0.0, value=25, step=0.1, format="%.2f")
+    POINTS_PER_CARVING_KNIFE = st.number_input("琢玉刀兑换比例", min_value=0.0, value=125, step=1.0, format="%.2f")
+    POINTS_PER_UNPOLISHED_JADE = st.number_input("璞玉兑换比例", min_value=0.0, value=25, step=0.1, format="%.2f")
     
     st.subheader("等级差设置")
     st.caption("步兵等级比弓兵高多少级？")
