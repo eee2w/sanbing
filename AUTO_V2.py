6544f6ed98eadc10bb0e0a3f6ae5c29ba6ce9e4b
eee2w <909623744@qq.com>
2026-05-03 20:04:29 +0800

    Update AUTO_V2.py

--- AUTO_V2.py
+++ AUTO_V2.py
@@ -128,10 +128,10 @@ with st.sidebar:
     
     st.subheader("兑换比例")
     POINTS_PER_WOOD = st.number_input("木头兑换比例", min_value=0.0, value=0.25, step=0.1, format="%.3f")
-    POINTS_PER_MITHRIL = st.number_input("精金兑换比例", min_value=0.0, value=5, step=0.1, format="%.2f")
-    POINTS_PER_LAPIS = st.number_input("青金石兑换比例", min_value=0.0, value=15, step=0.1, format="%.2f")
-    POINTS_PER_CARVING_KNIFE = st.number_input("琢玉刀兑换比例", min_value=0.0, value=75, step=1.0, format="%.2f")
-    POINTS_PER_UNPOLISHED_JADE = st.number_input("璞玉兑换比例", min_value=0.0, value=15, step=0.1, format="%.2f")
+    POINTS_PER_MITHRIL = st.number_input("精金兑换比例", min_value=0.0, value=5.0, step=0.1, format="%.2f")
+    POINTS_PER_LAPIS = st.number_input("青金石兑换比例", min_value=0.0, value=15.0, step=0.1, format="%.2f")
+    POINTS_PER_CARVING_KNIFE = st.number_input("琢玉刀兑换比例", min_value=0.0, value=75.0, step=1.0, format="%.2f")
+    POINTS_PER_UNPOLISHED_JADE = st.number_input("璞玉兑换比例", min_value=0.0, value=15.0, step=0.1, format="%.2f")
     
     st.subheader("等级差设置")
     st.caption("步兵等级比弓兵高多少级？")
